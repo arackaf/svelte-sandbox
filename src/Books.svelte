d0d026b76b705cbc07bd75269c140ff3bec9bc98
Adam Rackis <adamrackis@gmail.com>
2020-10-18 20:37:09 -0500

    use await block

--- src/Books.svelte
+++ src/Books.svelte
@@ -1,40 +1,42 @@
 <script>
   let title = "";
-  let books = [];
-  let loaded = false;
-  let loading = false;
+  let books = null;
 
-  const query = (title) => `{allBooks(PAGE:1, PAGE_SIZE: 10, SORT: { title: 1 }, title_contains: "${title}") { Books { title } }}`;
+  const query = title => `{allBooks(PAGE:1, PAGE_SIZE: 10, SORT: { title: 1 }, title_contains: "${title}") { Books { title } }}`;
 
-  async function search() {
-    loading = true;
-
+  async function getBooks() {
     let res = await fetch(`https://mylibrary.io/graphql-public?query=${encodeURIComponent(query(title))}`);
     let js = await res.json();
-    let results = js?.data?.allBooks?.Books ?? [];
-
-    loaded = true;
-    books = results;
-    loading = false;
+    return js?.data?.allBooks?.Books ?? [];
   }
+
 </script>
 
 <div>
   <input bind:value={title} />
-  <button on:click={search}>Go</button>
+  <button on:click={() => books = getBooks()}>Go</button>
 
   <div style="margin-top: 50px;">
-    {#if loading}<span>LOADING</span>{/if}
-    {#if loaded}
-      {#if books.length}
-        <div>
-          {#each books as book}
-            <div>{book.title}</div>
-          {/each}
-        </div>
-      {:else}
-        <div>No Results</div>
-      {/if}
-    {:else}<span>Search for some shit</span>{/if}
+    {#if books == null}
+      <span>Search for some stuff</span>
+    {:else}
+      {#await books}
+        <span>LOADING</span>
+      {:then books}
+        {#if books.length}
+          <div>
+            {#each books as book}
+              <div>{book.title}</div>
+            {/each}
+          </div>
+        {:else}
+          <div>No Results</div>
+        {/if}
+      {:catch}
+        <span>Shit Failed Yo</span>
+      {/await}
+    {/if}
+
+    <hr />
   </div>
 </div>
