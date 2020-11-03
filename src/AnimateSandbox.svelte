66a7426fce29c9468db8c1364edb5748aa013f25
Adam Rackis <adamrackis@gmail.com>
2020-11-03 10:11:09 -0600

    getting animation values straight

--- src/AnimateSandbox.svelte
+++ src/AnimateSandbox.svelte
@@ -1,35 +1,36 @@
 <script>
   import { spring, springFrames } from "svelte/motion";
 
-  let height = 0;
+  let height = 180;
   let open = false;
 
   const openSpringConfig = {
-    stiffness: 0.5,
+    stiffness: 0.1,
     damping: 0.35,
+    precision: 0.01
   };
   const closeSpringConfig = {
     stiffness: 0.4,
     damping: 1,
   };
 
-  let heightSpring = spring(-30, {});
+  let heightSpring = spring(0, {});
 
-  let X = springFrames(-30, 0, openSpringConfig);
-  debugger;
+  let X = springFrames(-30, 0, { stiffness: 0.1, damping: 0.35, precision: 0.01 });
+  let Y = springFrames(-30, 0, { stiffness: 1, damping: 1, precision: 0.01 });
 
-  $: heightSpring.set(open ? height : -30);
+  $: heightSpring.set(open ? height : 0);
   $: Object.assign(heightSpring, open ? openSpringConfig : closeSpringConfig);
 
   let arr = [];
-  $: arr.push($heightSpring);
-  $: console.log($heightSpring);
+  //$: arr.push($heightSpring);
+  //$: console.log($heightSpring);
 
-  window.log = () => console.log(arr.join(", "))
-
-
+  console.log("frames X", X);
+  console.log("frames 2", Y);
+  console.log("frames", X.join(","));
 </script>
 
-<button on:click={() => (open = !open)}>Toggle</button>
+<button on:click={() => (open = !open)}>Toggle Height</button>
 
-<div style={`margin-left: 80px; background-color: pink; width: 300px; height: 30px; transform: translate3d(0px, ${$heightSpring}px, 0px);`} />
+<div style={`margin-left: 80px; background-color: pink; width: 300px; height: ${$heightSpring}px;`} />
