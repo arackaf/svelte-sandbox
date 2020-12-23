26e25a2f145bc94682713b1ac0de7d3ad02fb861
Adam Rackis <adamrackis@hotmail.com>
2020-12-23 14:35:36 -0600

    web component / sass setup

--- index.js
+++ index.js
@@ -1,14 +1,2 @@
-import App from "./src/App";
+import Component from "./src/App.svelte";
 
-import { Client, setDefaultClient } from "./src/micro-graphql-react/index";
-
-const client = new Client({
-  endpoint: "https://mylibrary.io/graphql-public"
-});
-
-setDefaultClient(client);
-
-const app = new App({
-	target: document.getElementById("home"),
-	props: {}
-});
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,8 @@ const isProd = process.env.NODE_ENV == "production";
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
+const sass = require("svelte-preprocess-sass").sass;
+
 module.exports = [
   {
     entry: {
@@ -45,119 +47,11 @@ module.exports = [
             {
               loader: "svelte-loader",
               options: {
-                emitCss: true
-              }
-            }
-          ]
-        },
-        {
-          test: /\.(t|j)sx?$/,
-          exclude: /node_modules/,
-          loader: "babel-loader",
-          query: {
-            presets: ["@babel/preset-typescript"],
-            plugins: [
-              "@babel/plugin-proposal-class-properties",
-              "@babel/plugin-syntax-dynamic-import",
-              "@babel/plugin-proposal-optional-chaining",
-              "@babel/plugin-proposal-nullish-coalescing-operator"
-            ]
-          }
-        },
-        {
-          test: /\.graphql$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "generic-persistgraphql/loader",
-            options: {
-              path: path.resolve(__dirname, "extracted_queries.json")
-            }
-          }
-        },
-        {
-          test: /\.s?css$/,
-          oneOf: [
-            {
-              test: /\.module\.s?css$/,
-              use: [MiniCssExtractPlugin.loader, { loader: "css-loader", options: { modules: true } }, "sass-loader"]
-            },
-            {
-              use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
-            }
-          ]
-        },
-        {
-          test: /\.(png|jpg|gif|svg|eot|woff|woff2|ttf)$/,
-          use: [
-            {
-              loader: "file-loader"
-            }
-          ]
-        }
-      ]
-    },
-    optimization: {
-      //minimize: false
-    },
-    plugins: [
-      new HtmlWebpackPlugin({ template: "default.htm" }),
-      new MiniCssExtractPlugin({ filename: isProd ? "[name]-[contenthash].css" : "[name].css" })
-      //new BundleAnalyzerPlugin({ analyzerMode: "static" }),
-    ].filter(p => p),
-    stats: {
-      all: false,
-      assets: true,
-      modules: true,
-      maxModules: 10,
-      errors: true,
-      warnings: true,
-      moduleTrace: true,
-      errorDetails: true,
-      warningsFilter: warning => !/mini-css-extract-plugin.*conflicting order between/i.test(warning)
-    }
-  },
-  {
-    entry: {
-      mainServer: "./indexServer.js"
-    },
-    output: {
-      filename: isProd ? "[name]-bundle-[contenthash].js" : "[name]-bundle.js",
-      chunkFilename: isProd ? "[name]-chunk-[contenthash].js" : "[name]-chunk.js",
-      path: path.resolve(__dirname, "dist"),
-      publicPath: "/dist/"
-    },
-    resolve: {
-      extensions: [".mjs", ".ts", ".tsx", ".js", ".svelte"],
-      alias: {
-        svelte: path.resolve("node_modules", "svelte")
-      },
-      modules: [path.resolve("./"), path.resolve("./node_modules")],
-      mainFields: ["svelte", "browser", "module", "main"]
-    },
-    mode: isProd ? "production" : "development",
-    module: {
-      rules: [
-        {
-          test: /\.(html|svelte)$/,
-          exclude: /node_modules/,
-          use: [
-            {
-              loader: "babel-loader",
-              query: {
-                presets: ["@babel/preset-typescript"],
-                plugins: [
-                  "@babel/plugin-proposal-class-properties",
-                  "@babel/plugin-syntax-dynamic-import",
-                  "@babel/plugin-proposal-optional-chaining",
-                  "@babel/plugin-proposal-nullish-coalescing-operator"
-                ]
-              }
-            },
-            {
-              loader: "svelte-loader",
-              options: {
                 emitCss: true,
-                hydratable: true
+                customElement: true,
+                preprocess: {
+                  style: sass({}, { name: 'scss' })
+                }
               }
             }
           ]
@@ -212,7 +106,7 @@ module.exports = [
       //minimize: false
     },
     plugins: [
-      new HtmlWebpackPlugin({ template: "defaultServer.htm", filename: "indexServer.html" }),
+      new HtmlWebpackPlugin({ template: "default.htm", inject: "head" }),
       new MiniCssExtractPlugin({ filename: isProd ? "[name]-[contenthash].css" : "[name].css" })
       //new BundleAnalyzerPlugin({ analyzerMode: "static" }),
     ].filter(p => p),
