14ba29da611cb93ef62a5c15faae2078d033d29d
Adam Rackis <adamrackis@hotmail.com>
2021-01-20 21:41:35 -0600

    untracked demo start

---
+++ svelte-training/untracked.svelte
@@ -0,0 +1,26 @@
+<script>
+	let x = 0;
+	let y = 0;
+	
+	let xPlusY = 0;
+	$: {
+		xPlusY = x + y;
+	}
+</script>
+
+<button on:click={() => x++}>
+	Inc X
+</button>
+
+<button on:click={() => y++}>
+	Inc Y
+</button>
+
+<div>
+	x = {x}
+	<br />
+	y = {y}
+	<br />
+	xPlusY = {xPlusY}	
+	
+</div>
