f1929e2a50506f66db6b5d6de763203cb9b80dff
Adam Rackis <adamrackis@hotmail.com>
2021-01-30 13:30:24 -0600

    web component work

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,41 @@
+<svelte:options tag="my-element" />
+
+<script>
+  import { onMount, tick } from "svelte";
+
+  let rootEl;
+
+  onMount(() => {
+    let observer;
+    tick().then(() => {
+      let host = rootEl.parentNode.host;
+      let val = host.getAttribute("val");
+      let val2 = host.getAttribute("my-val");
+
+      console.log("init", { val, val2 });
+
+      let config = { attributes: true, childList: false, subtree: false };
+      observer = new MutationObserver(() => {
+        let val = host.getAttribute("val");
+        let val2 = host.getAttribute("my-val");
+
+        console.log("attr update", { val, val2 });
+      });
+      observer.observe(host, config);
+    });
+
+    return () => {
+      observer.disconnect();
+    };
+  });
+</script>
+
+<div bind:this={rootEl} class="root">
+  <h1>Hi there BEFORE SLOT</h1>
+  <slot />
+  <h1>Hi there AFTER SLOT</h1>
+</div>
+
 <style type="scss">
   .root {
     h1 {
@@ -5,9 +43,3 @@
     }
   }
 </style>
-
-<svelte:options tag="my-element" />
-
-<div class="root">
-  <h1>Hi there</h1>
-</div>
